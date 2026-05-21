bb7e97d0281f9d22fc3c599a11d0f067de4e72f2
Coder_Murphy <54139889+coder-murphy@users.noreply.github.com>
2026-05-21 17:02:42 +0800

    Create AutoMapperProfile for entity mapping
    Add AutoMapperProfile class for mapping configurations

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+
+namespace Mph.EFCore.Infrastructure
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            // 数据库实体 → DTO
+
+            // DTO → 数据库实体（新增/修改用）
+        }
+    }
+}
