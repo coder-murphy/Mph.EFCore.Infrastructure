fb2c7bb97807941eaaad1489f1ed1a7750aa2a05
Coder_Murphy <54139889+coder-murphy@users.noreply.github.com>
2026-05-21 17:51:31 +0800

    Add EDataActions enum for data operation types

---
+++ Defined/EDataActions.cs
@@ -0,0 +1,23 @@
+namespace Mph.EFCore.Infrastructure.Defined
+{
+    /// <summary>
+    /// 数据操作类型
+    /// </summary>
+    public enum EDataActions
+    {
+        /// <summary>
+        ///  创建
+        /// </summary>
+        INSERT,
+
+        /// <summary>
+        /// 更新
+        /// </summary>
+        UPDATE,
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        DELETE,
+    }
+}
